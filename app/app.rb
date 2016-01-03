8cf3e035fba7c79362962b0417352f8238155585
Hamid Zade <hamidmamadzade@gmail.com>
2016-01-03 05:37:52 +0000

    multiple tags implemented (and tested)

--- app/app.rb
+++ app/app.rb
@@ -1,7 +1,7 @@
 ENV["RACK_ENV"] ||= "development"
 
 require 'sinatra/base'
-require_relative 'models/link'
+require_relative 'data_mapper_setup'
 
 class BookmarkManager < Sinatra::Base
   get '/links' do
@@ -10,12 +10,22 @@ class BookmarkManager < Sinatra::Base
   end
 
   get '/links/new' do
-    erb :'links/new'
+    erb(:'links/new')
   end
 
   post '/links' do
-    Link.create(url: params[:url], title: params[:title])
+    link = Link.create(url: params[:url], title: params[:title])
+    params[:tags].split.each do |tag|
+      link.tags << Tag.create(name: tag)
+    end
+    link.save
     redirect to('/links')
+  end
+
+  get '/tags/:name' do
+    tag = Tag.first(name: params[:name])
+    @links = tag ? tag.links : []
+    erb(:'links/index')
   end
   
   run! if app_file == $0
--- spec/features/creating_links_spec.rb
+++ spec/features/creating_links_spec.rb
@@ -1,11 +1,9 @@
 feature 'Creating links' do
-
   scenario 'I can create a new link' do
     visit '/links/new'
     fill_in 'url',   with: 'http://www.zombo.com/'
     fill_in 'title', with: 'This is Zombocom'
     click_button 'Create link'
-
     expect(current_path).to eq '/links'
 
     within 'ul#links' do
